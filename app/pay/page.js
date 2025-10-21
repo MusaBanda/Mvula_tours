191dff30de4286611771ebbeaacd75a4946a72dd
MusaBanda <mosaro404@gmail.com>
2025-10-21 14:27:11 +0200

    json file crash fix

---
+++ app/pay/page.js
@@ -0,0 +1,13 @@
+
+
+export default function PayPage() {
+
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <p className=" w-[70vw] text-[5vh] text-red underline">
+        <span className="text-[red] e" >ERROR:</span> 
+        "SYSTEM MAINTENANCE PLEASE CHECK BACK LATER OR CONTACT SUPPORT"</p>
+    </div>
+  );
+}
+
